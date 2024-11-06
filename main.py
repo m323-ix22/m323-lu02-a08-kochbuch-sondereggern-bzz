b127e39ffd522046316d071f2ccae0809c8be599
sondereggern@bzz.ch <sondereggern@bzz.ch>
2024-11-06 09:13:51 +0100

    v

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-# Dein Code kommt hier hin
+import json
 
+# Funktion zum Anpassen der Rezeptmengen basierend auf der Anzahl der Personen
+def adjust_recipe(recipe, num_people):
+    factor = num_people / recipe["servings"]
+    adjusted_ingredients = {
+        ingredient: int(amount * factor)  # int() für ganze Zahlen bei den Mengen
+        for ingredient, amount in recipe["ingredients"].items()
+    }
+    # Rückgabe eines neuen, angepassten Rezepts mit der aktualisierten Portionenzahl
+    return {
+        "title": recipe["title"],
+        "ingredients": adjusted_ingredients,
+        "servings": num_people
+    }
+
+# Funktion zum Laden des Rezepts aus einem JSON-String
+def load_recipe(json_string):
+    return json.loads(json_string)
+
+# Hauptbereich des Programms
 if __name__ == '__main__':
-    # Beispiel für die Datenstruktur eines Rezepts
+    # Beispiel JSON für ein Rezept
     recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
-    # Dein Code kommt hier hin
+    # Rezept laden
+    recipe = load_recipe(recipe_json)
+    # Rezept anpassen
+    adjusted_recipe = adjust_recipe(recipe, 2)
+    print("Angepasstes Rezept:", adjusted_recipe)
